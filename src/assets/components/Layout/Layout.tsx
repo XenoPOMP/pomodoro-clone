2d841e19bb1a676a06cddf4fe67b95fd4616bdec
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-02-14 21:57:00 +0300

    📱 setup layout grid
    Took 11 minutes

--- src/assets/components/Layout/Layout.tsx
+++ src/assets/components/Layout/Layout.tsx
@@ -1,11 +1,14 @@
 import { PropsWith } from '@xenopomp/advanced-types';
 
+import cn from 'classnames';
 import { FC } from 'react';
 
 import GlobalProvider from '@providers/GlobalProvider/GlobalProvider';
 
+import Footer from '@ui/Footer/Footer';
 import Header from '@ui/Header/Header';
 
+import styles from './Layout.module.scss';
 import { LayoutProps } from './Layout.props';
 
 /**
@@ -17,9 +20,13 @@ import { LayoutProps } from './Layout.props';
 const Layout: FC<PropsWith<'children', LayoutProps>> = ({ children }) => {
   return (
     <GlobalProvider>
-      <Header />
+      <div className={cn(styles.wrapper)}>
+        <Header />
 
-      <main>{children}</main>
+        <main>{children}</main>
+
+        <Footer />
+      </div>
     </GlobalProvider>
   );
 };
