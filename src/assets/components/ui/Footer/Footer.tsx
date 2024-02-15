0856340f398863ea5bc9852c7b18243b681d879a
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-02-15 09:45:20 +0300

    ⚙️ add settings button to footer
    Took 5 minutes

--- src/assets/components/ui/Footer/Footer.tsx
+++ src/assets/components/ui/Footer/Footer.tsx
@@ -1,11 +1,33 @@
+import { VariableFC } from '@xenopomp/advanced-types';
+
 import cn from 'classnames';
+import { Cog } from 'lucide-react';
 import { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import Pressable from '@ui/Pressable/Pressable';
 
 import styles from './Footer.module.scss';
 import type { FooterProps } from './Footer.props';
 
-const Footer: FC<FooterProps> = ({}) => {
-  return <footer className={cn(styles.appFooter)}></footer>;
+const Footer: VariableFC<'footer', FooterProps, 'children'> = ({
+  className,
+  ...props
+}) => {
+  const navigate = useNavigate();
+
+  return (
+    <footer className={cn(styles.appFooter, className)} {...props}>
+      <Pressable
+        onClick={() => {
+          // TODO Add redirection to settings
+          // navigate('/settings');
+        }}
+      >
+        <Cog width={'.75em'} height={'.75em'} />
+      </Pressable>
+    </footer>
+  );
 };
 
 export default Footer;
